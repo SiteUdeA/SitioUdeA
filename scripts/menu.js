713395975df52c0fc02b555747790b0a21bbe202
JulianEscobarLucio <attilaescobar@hotmail.com>
2016-07-08 00:03:23 -0500

    Agrego menu desde script

---
+++ scripts/menu.js
@@ -0,0 +1,40 @@
+
+
+window.addEventListener("load", function(){
+    document.getElementById('templatemo_header').innerHTML = '<div id="site_title"><h1><a href="#">Free CSS Templates</a></h1></div>\
+    <div id="templatemo_menu" class="ddsmoothmenu">\
+            <ul>\
+                <li><a href="index.html" class="selected">Home</a></li>\
+              <li><a href="about.html">About</a>\
+                    <ul>\
+                        <li><a href="#">Background</a></li>\
+                        <li><a href="#">Our Team</a></li>\
+                        <li><a href="#">Management</a></li>\
+                    </ul>\
+                </li>\
+              <li><a href="portfolio.html">Portfolio</a>\
+                    <ul>\
+                        <li><a href="#">Web Design</a></li>\
+                        <li><a href="#">Multimedia</a></li>\
+                        <li><a href="#">3D Animations</a></li>\
+                        <li><a href="#">Templates</a></li>\
+                    </ul>\
+                </li>\
+                <li><a href="blog.html">Blog</a>\
+                  <ul>\
+                        <li><a href="#">Useful Tips</a></li>\
+                        <li><a href="#">Archives</a></li>\
+                        <li><a href="#">Popular Posts</a></li>\
+                        <li><a href="#">Comments</a></li>\
+                        <li><a href="#">Authors</a></li>\
+                    </ul>\
+                </li>\
+                <li><a href="contact.html">Contact</a></li>\
+            </ul>\
+            <br style="clear: left" />\
+        </div> ';        
+        
+}, false);
+
+
+
