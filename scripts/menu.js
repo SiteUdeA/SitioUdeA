ef73c48056f9438db661faaad1914af2e7af6806
JulianEscobarLucio <attilaescobar@hotmail.com>
2016-07-09 00:30:39 -0500

    .

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,16 @@
 
 
 window.addEventListener("load", function(){
-    document.getElementById('templatemo_header').innerHTML = '<div id="site_title"><h1><a href="#">Free CSS Templates</a></h1></div>\
-    <div id="templatemo_menu" class="ddsmoothmenu">\
-            <ul>\
-                <li><a href="index.html" class="selected">Home</a></li>\
-              <li><a href="about.html">About</a>\
+    document.getElementById('templatemo_menu').innerHTML = '<ul>\
+                <li><a href="index.html" >Home</a></li>\
+              <li><a href="about.html">Research</a>\
                     <ul>\
                         <li><a href="#">Background</a></li>\
                         <li><a href="#">Our Team</a></li>\
                         <li><a href="#">Management</a></li>\
                     </ul>\
                 </li>\
-              <li><a href="portfolio.html">Portfolio</a>\
+              <li><a href="portfolio.html">People</a>\
                     <ul>\
                         <li><a href="#">Web Design</a></li>\
                         <li><a href="#">Multimedia</a></li>\
@@ -31,10 +29,7 @@ window.addEventListener("load", function(){
                 </li>\
                 <li><a href="contact.html">Contact</a></li>\
             </ul>\
-            <br style="clear: left" />\
-        </div> ';        
+            <br style="clear: left" /> ';        
         
 }, false);
-
-
 
